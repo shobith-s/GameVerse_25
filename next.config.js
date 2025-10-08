2f6f1751777085e34c1dafdbed25bd5ba61f2ece
Shobith S <shobi7196@gmail.com>
2025-10-08 10:35:41 +0530

    fix: use next.config.js and remove duplicates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // don’t fail the Vercel build on lint or type errors
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  experimental: { serverActions: true },
+
+  experimental: {
+    serverActions: true,
+  },
 };
+
 module.exports = nextConfig;
